0692c6ed284318aed9079900ee954139672b20c9
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-09 17:59:09 +0100

    ..

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,8 +13,8 @@ namespace ConsoleApp
 
         private static void Main(string[] args)
         {
-            
-           // AddSamurai();
+
+            // AddSamurai();
             //GetSamurais("After Add:");
             InsertMultipleSamurais();
             Console.Write("Press any key...");
@@ -33,18 +33,23 @@ namespace ConsoleApp
             var samurai2 = new Samurai { Name = "Hbiba", Clan = clan1, Horse = horse2 };
             var horse3 = new Horse { Name = "ein sehr schon pferd" };
             var samurai3 = new Samurai { Name = "Mama", Clan = clan1, Horse = horse2 };
-            context.AddRange(samurai, samurai1,samurai2,samurai3);
+            context.AddRange(samurai, samurai1, samurai2, samurai3);
             context.SaveChanges();
         }
         private static void AddSamurai()
         {
             var horse = new Horse { Name = "pferd" };
             var clan = new Clan { ClanName = "shi clan" };
-            var samurai = new Samurai { Name = "Lekbedbed" , Clan = clan, Horse=horse};
+            var samurai = new Samurai { Name = "Lekbedbed", Clan = clan, Horse = horse };
             context.Samurais.Add(samurai);
             context.SaveChanges();
         }
-
+        private static void GetSamuraisSimpler() 
+        {
+            //var samurais = context.Samurais.ToList();
+            var query = context.Samurais;
+            var samurais = query.ToList();
+        }
         private static void GetSamurais(string text)
         {
             var samurais = context.Samurais.ToList();
