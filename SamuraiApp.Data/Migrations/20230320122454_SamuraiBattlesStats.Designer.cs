b28df028472cdf4929f45a7636d861ded57ceb66
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-30 11:40:29 +0000

    ..

--- SamuraiApp.Data/Migrations/20230320122454_SamuraiBattlesStats.Designer.cs
+++ SamuraiApp.Data/Migrations/20230320122454_SamuraiBattlesStats.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
@@ -11,9 +12,11 @@ using SamuraiApp.Data;
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20230320122454_SamuraiBattlesStats")]
+    partial class SamuraiBattlesStats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
